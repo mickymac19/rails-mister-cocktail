cf9937abb8bda690bb841096b8b95b99f6557fd2
Michelle Macato <michellefmacato@gmail.com>
2020-08-06 11:29:38 +0900

    added gems, bootstrap, ready to go

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+
 module.exports = environment
